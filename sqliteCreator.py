b38c529a543a79d09c410e6a2997ec8b4524ae6a
thutkowski <116215476+thutkowski@users.noreply.github.com>
2023-10-07 18:11:35 -0700

    Connected form to sqlite db

--- sqliteCreator.py
+++ sqliteCreator.py
@@ -1,4 +1,10 @@
 import sqlite3
 
-cur = sqlite3.connect("DND.db")
+cur = sqlite3.connect("/Users/Tim/Documents/DNDproject/dNdProject/dNdProject/bin/Debug/DND.db")
 
+cur.execute("CREATE TABLE tbl1 (id, name, age )")
+cur.execute("""INSERT INTO tbl1 (id, name, age ) VALUES (1,"Tim",20)""")
+cur.execute("""INSERT INTO tbl1 (id, name, age ) VALUES (2,"Marcos",20)""")
+cur.execute("""INSERT INTO tbl1 (id, name, age ) VALUES (3,"Zach",20)""")
+cur.commit()
+cur.close()
