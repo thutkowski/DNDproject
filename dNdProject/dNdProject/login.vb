248950c2315b15b051af58f0eb50a710a549bb8b
thutkowski <116215476+thutkowski@users.noreply.github.com>
2023-10-25 20:27:24 -0700

    adding a new form maybe messing things up

--- dNdProject/dNdProject/login.vb
+++ dNdProject/dNdProject/login.vb
@@ -8,28 +8,16 @@ Public Class login
 
     Private Sub loginActionButton_Click(sender As Object, e As EventArgs) Handles loginActionButton.Click
         characterUser = characterTextBox.Text
-        connection.Open()
 
-        command.Connection = connection
-        command.CommandText = "SELECT * FROM characters WHERE characterName =@characterUser"
-        command.Parameters.AddWithValue("@characterUser", characterTextBox.Text)
-
-
-
-        rdr = command.ExecuteReader()
-        'Using rdr
-        '    While (rdr.Read())
-        '        ((rdr.GetInt32(0) & rdr.GetString(1) & rdr.GetInt32(2)))
-        '    End While
-        'End Using
-        If rdr.Read() Then
+        If loginFunction(characterUser) = True Then
             characterSheet.Show()
         Else
             MessageBox.Show("That character does not exist")
         End If
-        connection.Close()
-        If connection.State = ConnectionState.Open Then
-            MsgBox("The connection is: " & connection.State.ToString)
-        End If
+
+    End Sub
+
+    Private Sub createUserButton_Click(sender As Object, e As EventArgs) Handles createUserButton.Click
+        characterSheet.Show()
     End Sub
 End Class
